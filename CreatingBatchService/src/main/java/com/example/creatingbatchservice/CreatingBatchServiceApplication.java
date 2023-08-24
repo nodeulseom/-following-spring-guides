0796fa102ec9ddde2c347d66b75ff967cc6796db
nodeulseom <hyunnov04@gmail.com>
2023-08-24 15:30:28 +0900

    배치서비스

---
+++ CreatingBatchService/src/main/java/com/example/creatingbatchservice/CreatingBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.creatingbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingBatchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatingBatchServiceApplication.class, args);
+    }
+
+}
